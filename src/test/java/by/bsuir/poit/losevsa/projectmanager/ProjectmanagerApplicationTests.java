0e2b744a0cbffa8f2a050f2bc32228a81590cf83
MerakseS <lsnwot@gmail.com>
2022-05-21 20:01:23 +0300

    Initial commit

---
+++ src/test/java/by/bsuir/poit/losevsa/projectmanager/ProjectmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.poit.losevsa.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
